30dfbc5ae312ce7861d2b42a02e0a46f8c37d4a2
Diego <clickalvon@gmail.com>
2025-09-09 10:55:37 -0300

    Corrigir base do Vite para GitHub Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/sistema-kg-amor-frontend/' : '/',
+  // Base correta para GitHub Pages:
+  base: '/sistema-kg-amor-frontend/',
   build: {
-    outDir: 'dist',
+    outDir: 'dist'
   },
   server: {
-    host: true,
     port: 3000,
-  },
+    host: true
+  }
 })
