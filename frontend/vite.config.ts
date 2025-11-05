851a90d50b492ff85aa1787806ef9cfa17ee42c9
Diego <clickalvon@gmail.com>
2025-11-05 16:55:19 -0300

    AJUSTE ENDEREÇO

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/', // <--- ADICIONE ESTA LINHA
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/sistema-kg-amor-frontend/' : '/',
   build: {
